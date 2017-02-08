7e9d1b209c827d9a0a938f4df2c8222efc33c7b2
bigmuflar <jmdegonda@gmail.com>
2017-02-08 11:37:04 -0700

    update

--- content.js
+++ content.js
@@ -4,3 +4,18 @@ var app = new Vue({
     message: 'Hello Vue!'
   }
 })
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { text: 'Vegetables' },
+      { text: 'Cheese' },
+      { text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
